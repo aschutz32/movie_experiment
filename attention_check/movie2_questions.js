8e596ea4b89f7aa5fc235c10c888122eb5f6c55b
Alisa <aschutz@uchicago.edu>
2023-02-20 21:15:11 -0600

    2.20.23

---
+++ attention_check/movie2_questions.js
@@ -0,0 +1,25 @@
+
+function movie2AttentionCheck(){
+    var trial = {
+        type: jsPsychSurveyMultiChoice,
+        questions: [
+            {
+                prompt: "<b>Which clip appeared in the videos you watched? </b>",
+                name: 'ScenesNotM1',
+                options: ['clip about a community garden', 'clip about items in pockets', 'clip about a hotel stay', 'clip about planning a money heist', 'clip about being left at home alone', 'clip about a flower'],
+                required: true
+            },
+        ],
+        on_finish: function(data){
+            const resp = data.response['volume_adjust'];
+          if(resp == "clip about items in pockets"){
+            data.correct = true;
+          } else {
+            data.correct = false;
+          };
+        },
+        data: {TASK: 'AttentionCheck'},
+
+    };
+    return trial;
+}
